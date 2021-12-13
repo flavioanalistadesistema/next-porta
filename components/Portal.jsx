db725e374448aaa0017ec38ee0c23297a14554e0
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-12-13 20:09:57 -0300

    feat: Component Porta #4

--- components/Portal.jsx
+++ components/Portal.jsx
@@ -2,8 +2,13 @@ import styles from '../styles/Porta.module.css'
 
 export default function Porta() {
     return (
-        <div className={styles.porta}>
-            <div className={styles.numero}>3</div>          
+        <div className={styles.area}>
+            <div className={styles.estrutura}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>3</div>
+                </div>
+            </div>
         </div>
+
     )
 }
