d8be8c9149e28cd1d16a9d3210e7d6739f4b43c7
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-12-17 14:23:25 -0300

    feat: Component PORTAL #5

--- components/Portal.jsx
+++ components/Portal.jsx
@@ -6,6 +6,7 @@ export default function Porta() {
             <div className={styles.estrutura}>
                 <div className={styles.porta}>
                     <div className={styles.numero}>3</div>
+                    <div className={styles.masaneta}></div>
                 </div>
             </div>
             <div className={styles.chao}></div>
