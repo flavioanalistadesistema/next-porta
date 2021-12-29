d2abf8c3cbd9d9e5fe4a1c189ee7252d7da3a799
Flavio Santosm <flavioanalistadesistema@gmail.com>
2021-12-29 09:01:41 -0300

    feat: criando button reinicia jogo

--- pages/jogo.tsx
+++ pages/jogo.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/Jogo.module.css"
 import { useState } from "react"
 import Porta from "../components/Portal"
 import { atualizarPorta, criarPortas } from "../functons/portas"
+import Link from "next/link"
 
 export default function Jogo() {
     const [portas, setPorta] = useState(criarPortas(4, 3))
@@ -18,6 +19,11 @@ export default function Jogo() {
         <div className={styles.jogo}>
             <div className={styles.portas}>
                 {renderizarPorta()}
+            </div>
+            <div className={styles.botoes}>
+                <Link href={'/'}>
+                    <button>Reiniciar Jogo</button>
+                </Link>
             </div>
         </div>
     )
